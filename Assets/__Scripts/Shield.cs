3c73f9d197abb751a8466fc7a135adbd50affa8c
Erickson Silva <erickson.lucas@live.com>
2017-12-26 00:09:46 -0200

    Game in progress.

---
+++ Assets/__Scripts/Shield.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public float rotationPerSecond = 0.1f;
+    [Header("Set Dynamically")]
+    public int levelShow = 0;
+    Material mat;
+
+	void Start () {
+        mat = GetComponent<Renderer>().material;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+        if (levelShow != currLevel)
+        {
+            levelShow = currLevel;
+            mat.mainTextureOffset = new Vector2(0.2f * levelShow, 0);
+        }
+        float rZ = -(rotationPerSecond * Time.time * 3600) % 360f;
+        transform.rotation = Quaternion.Euler(0, 0, rZ);
+    }
+}
