c773643fdbe581bd846dd3ae26538b4d8a3a84ad
Erickson Silva <erickson.lucas@live.com>
2017-12-27 22:33:22 -0200

    Game finished.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -9,8 +9,13 @@ public class Hero : MonoBehaviour {
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public float gameRestartDelay = 2f;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
     [Header("Set Dynamically")]
-    public float shieldLevel = 1;
+    private GameObject lastTriggerGo = null;
+    [SerializeField]
+    private float _shieldLevel = 1;
 
     private void Awake()
     {
@@ -37,12 +42,54 @@ public class Hero : MonoBehaviour {
         pos.y += yAxis * speed * Time.deltaTime;
         transform.position = pos;
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TempFire();
+        }
 	}
+
+    void TempFire()
+    {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.velocity = Vector3.up * projectileSpeed;
+    }
+
+    public float shieldLevel
+    {
+        get
+        {
+            return (_shieldLevel);
+        }
+        set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+            if (value < 0)
+            {
+                Destroy(this.gameObject);
+                Main.S.DelayedRestart(gameRestartDelay);
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Transform rootT = other.gameObject.transform.root;
         GameObject go = rootT.gameObject;
-        print("Triggered: " + go.gameObject.name);
+        if (go == lastTriggerGo)
+        {
+            return;
+        }
+        lastTriggerGo = go;
+        if (go.tag == "Enemy")
+        {
+            shieldLevel--;
+            Destroy(go);
+        } 
+        else
+        {
+            print("Triggered by non-Enemy: " + go.name);
+        }
     }
 }
---
+++ Assets/__Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private BoundsCheck bndCheck;
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bndCheck.offUp)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
